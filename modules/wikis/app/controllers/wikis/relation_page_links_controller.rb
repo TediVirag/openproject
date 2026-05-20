e4caa5211b406ed5c85c4917684d25cd851c4bc5
Jan Sandbrink <j.sandbrink@openproject.com>
2026-05-20 15:43:06 +0200

    Allow creating relation page links through UI
    This change is mostly wiring up things, but leaves some things open:
    
    * we still need to use a proper create service (to be built)
    * the modal needs to use a treeview to select the identifier
      (requires fetching of a list of available wiki pages)

--- modules/wikis/app/controllers/wikis/relation_page_links_controller.rb
+++ modules/wikis/app/controllers/wikis/relation_page_links_controller.rb
@@ -32,21 +32,56 @@ module Wikis
   class RelationPageLinksController < ApplicationController
     include OpTurbo::ComponentStream
 
-    before_action :find_page_link
     before_action :authorize
 
+    def create
+      # TODO: Wikis::PageLinks::CreateService
+      page_link = RelationPageLink.create!(**relation_page_link_params)
+
+      path = derive_path_from_linkable(page_link.linkable)
+      return redirect_to path, status: :see_other if path
+
+      head :no_content
+    end
+
     def destroy
-      # TODO: implement delete service
+      # TODO: Wikis::PageLinks::DeleteService
+      page_link = find_page_link
+      page_link.destroy!
+
+      path = derive_path_from_linkable(page_link.linkable)
+      return redirect_to path, status: :see_other if path
+
+      head :no_content
     end
 
     def confirm_delete_dialog
-      respond_with_dialog(DeleteRelationPageLinkConfirmationDialog.new(page_link: @page_link))
+      page_link = find_page_link
+      respond_with_dialog(DeleteRelationPageLinkConfirmationDialog.new(page_link:))
+    end
+
+    def link_existing_dialog
+      linkable = WorkPackage.visible.find(params.expect(:work_package))
+      provider = Provider.visible.find(params.expect(:provider))
+      respond_with_dialog Wikis::LinkExistingWikiPageDialog.new(linkable:, provider:)
     end
 
     private
 
     def find_page_link
-      @page_link = RelationPageLink.find(params.expect(:id))
+      RelationPageLink.find(params.expect(:id))
+    end
+
+    def relation_page_link_params
+      params.expect(wikis_relation_page_link: %i[identifier provider_id linkable_type linkable_id])
+            .merge(author: current_user)
+    end
+
+    def derive_path_from_linkable(linkable)
+      case linkable
+      when WorkPackage
+        project_work_package_wikis_tab_index_path(work_package_id: linkable.id, project_id: linkable.project_id)
+      end
     end
   end
 end
